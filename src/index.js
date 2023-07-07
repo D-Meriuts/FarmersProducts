7f658e6574ef50453a1e297108d390445f4bd6a6
DMeriuts <dimameriuts@gmail.com>
2023-07-07 09:41:05 +0300

    изменения

--- src/index.js
+++ src/index.js
@@ -1,15 +1,31 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+// import { StrictMode } from "react";
+// import { ThemeProvider } from "styled-components";
+// import { defaultTheme } from "/src/thems/default";
+// import { createRoot } from "react-dom/client";
 
+// import App from "/src/components/app/app";
+
+// const rootElement = createRoot(document.getElementById("root"));
+// rootElement.render(
+//   <StrictMode>
+//     <ThemeProvider theme={defaultTheme}>
+//       <App />
+//     </ThemeProvider>
+//   </StrictMode>
+// );
+
+import { StrictMode } from "react";
+import ReactDOM from "react-dom";
 import App from "/src/components/app/app";
 import { ThemeProvider } from "styled-components";
-import { defaultTheme } from "./thems/default";
+import { defaultTheme } from "/src/thems/default";
 
-const rootElement = createRoot(document.getElementById("root"));
-rootElement.render(
+const rootElement = document.getElementById("root");
+ReactDOM.render(
   <StrictMode>
     <ThemeProvider theme={defaultTheme}>
       <App />
     </ThemeProvider>
-  </StrictMode>
+  </StrictMode>,
+  rootElement
 );
